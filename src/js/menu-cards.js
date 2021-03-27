8c4487b31707f8f25e547c6069f5c8a0d26eafdb
yevhen-cherniuk <chernuy1@gmail.com>
2021-03-27 17:20:09 +0300

    update menu-cards.js

--- src/js/menu-cards.js
+++ src/js/menu-cards.js
@@ -1,40 +1,32 @@
 import data from '../data/menu.json';
 import template from '../template/template.hbs';
-
+const refs = {
+  body: document.body,
+  list: document.getElementsByClassName('js-menu')[0],
+  input: document.querySelector('.theme-switch__toggle'),
+};
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-
-const refs = {
-    body: document.body,
-    list: document.querySelector('.js-menu'),
-    input: document.querySelector('.theme-switch__control'),
-}
 refs.body.classList.add(
-    localStorage.getItem('theme') === null ? Theme.LIGHT : localStorage.getItem('theme')
+  localStorage.getItem('theme') === null
+    ? Theme.LIGHT
+    : localStorage.getItem('theme'),
 );
-refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
-
-// refs.body.classList.add(Theme.LIGHT);
-
 const markup = template(data);
+refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
 refs.list.insertAdjacentHTML('beforeend', markup);
-
-function changeTheme(add, rem) {
-    refs.body.classList.remove(rem);
-        refs.body.classList.add(add);
-        localStorage.setItem('theme',add)
-    
-}
-
 refs.input.addEventListener('change', inputChangeOn);
-function inputChangeOn({ target }) {
-    if (target.checked) {
-        changeTheme(Theme.DARK, Theme.LIGHT);
-    }
-    else {
-        changeTheme( Theme.LIGHT , Theme.DARK);
-    }
+function changeTheme(add, rem) {
+  refs.body.classList.remove(rem);
+  refs.body.classList.add(add);
+  localStorage.setItem('theme', add);
 }
-
+function inputChangeOn({ target }) {
+  if (target.checked) {
+    changeTheme(Theme.DARK, Theme.LIGHT);
+  } else {
+    changeTheme(Theme.LIGHT, Theme.DARK);
+  }
+}
