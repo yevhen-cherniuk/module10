96ca6dc3e5cc9d911495a715fcca716b13113880
yevhen-cherniuk <chernuy1@gmail.com>
2021-03-27 18:09:55 +0300

    last update menu-cards.js

--- src/js/menu-cards.js
+++ src/js/menu-cards.js
@@ -1,32 +1,35 @@
+import { templates } from 'handlebars';
 import data from '../data/menu.json';
 import template from '../template/template.hbs';
+
 const refs = {
   body: document.body,
-  list: document.getElementsByClassName('js-menu')[0],
+  list: document.querySelector('.js-menu'),
   input: document.querySelector('.theme-switch__toggle'),
 };
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
 refs.body.classList.add(
   localStorage.getItem('theme') === null
     ? Theme.LIGHT
     : localStorage.getItem('theme'),
 );
-const markup = template(data);
 refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
+
+const markup = template(data);
 refs.list.insertAdjacentHTML('beforeend', markup);
 refs.input.addEventListener('change', inputChangeOn);
+
 function changeTheme(add, rem) {
-  refs.body.classList.remove(rem);
-  refs.body.classList.add(add);
+  refs.body.classList.replace(rem, add);
   localStorage.setItem('theme', add);
 }
 function inputChangeOn({ target }) {
-  if (target.checked) {
-    changeTheme(Theme.DARK, Theme.LIGHT);
-  } else {
-    changeTheme(Theme.LIGHT, Theme.DARK);
-  }
+  target.checked
+    ? changeTheme(Theme.DARK, Theme.LIGHT)
+    : changeTheme(Theme.LIGHT, Theme.DARK);
 }
